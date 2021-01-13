efc5bddd43d386db48f822d3d54cc3b430e4573e
SarahTruffert <77342348+SarahTruffert@users.noreply.github.com>
2021-01-13 09:50:21 +0100

    Update script exo.sh

--- script exo.sh
+++ script exo.sh
@@ -1,16 +1,10 @@
-#./nom_du_script.sh, faite la commande : sed -i -e 's/\r$//'  nom_du_script.sh
-#Pour lancer script 
 #!/bin/bash
-
 echo "UBUNTU POST-INSTALL SCRIPT"
 echo "Updating APT..."
 sudo apt-get update 
 clear
-#Pour intaller Git 
 echo "Installing base packages"
 sudo apt-get install --yes git git-extras build-essential python3-pip htop glances
-#Pour installer visual code 
 -O vscode.deb
 sudo dpkg -i ./vscode.deb
 sudo apt-get install -f
-#Pour installer code discord 
