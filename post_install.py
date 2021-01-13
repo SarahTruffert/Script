6e3bbad3139e39d46a0ce51039d7997582767e1b
SarahTruffert <77342348+SarahTruffert@users.noreply.github.com>
2021-01-13 12:01:18 +0100

    Update post_install.py

--- post_install.py
+++ post_install.py
@@ -1,12 +1,14 @@
 import os
 #!/bin/bash
-
+#echo comme print sur python
 print("UBUNTU POST-INSTALL SCRIPT")
 print("Updating APT...")
+#commentaire os systeme 
 os.system("sudo apt-get update") 
 os.system("clear")
 print("Installing base packages")
 os.system("sudo apt-get install --yes git git-extras build-essential python3-pip htop glances")
+os.system("wget https://go.microsoft.com/fwlink/?LinkID=760868")
 os.system("-O vscode.deb")
 os.system("sudo dpkg -i ./vscode.deb")
 os.system("sudo apt-get install -f")
